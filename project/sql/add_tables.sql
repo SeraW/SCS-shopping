c63560fc31af20d9e839a84f71f8d5cc6a5f9270
sera <sera.wong@live.ca>
2022-02-19 18:12:27 -0500

    auto increment

--- project/sql/add_tables.sql
+++ project/sql/add_tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS Users (
-    user_id INT PRIMARY KEY,
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(25) NOT NULL,
     last_name VARCHAR(25) NOT NULL,
     phone VARCHAR(25) NOT NULL,
