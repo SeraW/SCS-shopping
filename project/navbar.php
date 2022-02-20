970332a2f943634810fd05807dae69181b450037
David Tran <davidtran990@hotmail.com>
2022-02-20 12:49:05 -0500

    added functionality to nav

--- project/navbar.php
+++ project/navbar.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -15,7 +18,6 @@
 </head>
 
 <body>
-
 <nav class="z-depth-0">
     <div class="nav-wrapper">
       <a href="home.php" class="brand-logo">S<span class="material-icons" id="globe">travel_explore</span>S</a>
@@ -26,9 +28,27 @@
         <li><a href="types.php">Types of Services</a></li>
         <li><a href="reviews.php">Reviews</a></li>
         <li><a href="contact.php">Contact Us</a></li>
-        <li><a href="admin.php">Admin</a></li>
-        <li><a href="login.php">Sign In</a></li>
-        <li><a href="register.php">Sign Up</a></li>
+        <?php
+          if (isset($_SESSION["username"])){
+            $db = mysqli_connect("localhost", "root", "", "project");
+            if ($db->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+            $user = $_SESSION["username"];
+            $s = "SELECT admin_val FROM users WHERE login_id = '$user'";
+            $result = mysqli_query($db, $s);
+            $row = mysqli_fetch_row($result);
+            if ($row[0] == 1){
+              echo "<li><a href='admin.php'>Admin</a></li>";
+              echo "<li><a href='./validation/logout.php'>Logout</a></li>";
+            } else {
+              echo "<li><a href='./validation/logout.php'>Logout</a></li>";
+            }
+          } else{
+            echo "<li><a href='login.php'>Sign In</a></li>";
+            echo "<li><a href='register.php'>Sign Up</a></li>";
+          }
+        ?>
       </ul>
     </div>
   </nav>
@@ -39,9 +59,27 @@
         <li><a href="types.php">Types of Services</a></li>
         <li><a href="reviews.php">Reviews</a></li>
         <li><a href="contact.php">Contact Us</a></li>
-        <li><a href="admin.php">Admin</a></li>
-        <li><a href="login.php">Sign In</a></li>
-        <li><a href="register.php">Sign Up</a></li>
+        <?php
+          if (isset($_SESSION["username"])){
+            $db = mysqli_connect("localhost", "root", "", "project");
+            if ($db->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+            $user = $_SESSION["username"];
+            $s = "SELECT admin_val FROM users WHERE login_id = '$user'";
+            $result = mysqli_query($db, $s);
+            $row = mysqli_fetch_row($result);
+            if ($row[0] == 1){
+              echo "<li><a href='admin.php'>Admin</a></li>";
+              echo "<li><a href='./validation/logout.php'>Logout</a></li>";
+            } else {
+              echo "<li><a href='./validation/logout.php'>Logout</a></li>";
+            }
+          } else{
+            echo "<li><a href='login.php'>Sign In</a></li>";
+            echo "<li><a href='register.php'>Sign Up</a></li>";
+          }
+        ?>
     </ul>
           
 
