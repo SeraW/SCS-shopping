4562355beb9b08435d979e86765e3e4d0cb62060
David Tran <davidtran990@hotmail.com>
2022-02-19 21:29:35 -0500

    Minor fixes

--- project/about.php
+++ project/about.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Student Records</title>
+    <title>SCS | About Us</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--Import materialize.css-->
