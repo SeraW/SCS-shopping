85e111948b32b99281f626801de5b193e0f287cb
David Tran <davidtran990@hotmail.com>
2022-03-03 20:05:03 -0500

    fixed review page

--- project/reviews.php
+++ project/reviews.php
@@ -22,178 +22,177 @@ if ($db->connect_error) {
 }?>
 
 <body>
-
-<div class="container">
-    <div class="row" id="title">
-        <h1>What people are <span class="highlight">saying</span></h1>
-        <h2>We've had the pleasure of delivering to over 500 customers worldwide. Here's what they've had to say.<h2>
-    </div>
-</div>
-
-    <div class="carousel">
-
-        <div class="carousel-item">
-            <div class="card horizontal sticky-action">
-                <div class="card-stacked">
-                    <div class="card-content">
-                    <span class="material-icons star">star_rate star_rate star_rate star_rate star_rate</span>
-                    <?php
-                        try{
-                            $s = "SELECT review_text FROM review WHERE review_id = 1";
-                            $result = mysqli_query($db, $s);
-                            $row = mysqli_fetch_row($result);
-                            echo "<p>" . $row[0] . "</p>";
-                            echo '</div>
-                            <div class="card-action">';
-                            $name= "SELECT first_name, last_name FROM users u, review r WHERE review_id=1 AND u.user_id=r.user_id";
-                            $result = mysqli_query($db, $name);
-                            $row = mysqli_fetch_row($result);
-                            echo '<a href="#" style="color:#149BBB">' . $row[0] . " " . $row[1] . '</a>
-                            </div>';
-                        } catch(throwable $e){
-                            echo "<p>Error displaying review</p>";
-                        }
-                    ?>
-                </div>
-            </div>
-        </div>
-
-        <div class="carousel-item">
-            <div class="card horizontal sticky-action">
-                <div class="card-stacked">
-                    <div class="card-content">
-                    <span class="material-icons star">star_rate star_rate star_rate star_rate star_rate</span>
-                    <?php
-                        try{
-                            $s = "SELECT review_text FROM review WHERE review_id = 2";
-                            $result = mysqli_query($db, $s);
-                            $row = mysqli_fetch_row($result);
-                            echo "<p>" . $row[0] . "</p>";
-                            echo '</div>
-                            <div class="card-action">';
-                            $name= "SELECT first_name, last_name FROM users u, review r WHERE review_id=2 AND u.user_id=r.user_id";
-                            $result = mysqli_query($db, $name);
-                            $row = mysqli_fetch_row($result);
-                            echo '<a href="#" style="color:#149BBB">' . $row[0] . " " . $row[1] . '</a>
-                            </div>';
-                        } catch(throwable $e){
-                            echo "<p>Error displaying review</p>";
-                        }
-                    ?>
-                </div>
-            </div>
-        </div>
-
-        <div class="carousel-item">
-            <div class="card horizontal sticky-action">
-                <div class="card-stacked">
-                    <div class="card-content">
-                    <span class="material-icons star">star_rate star_rate star_rate star_rate star_rate</span>
-                    <?php
-                        try{
-                            $s = "SELECT review_text FROM review WHERE review_id = 3";
-                            $result = mysqli_query($db, $s);
-                            $row = mysqli_fetch_row($result);
-                            echo "<p>" . $row[0] . "</p>";
-                            echo '</div>
-                            <div class="card-action">';
-                            $name= "SELECT first_name, last_name FROM users u, review r WHERE review_id=3 AND u.user_id=r.user_id";
-                            $result = mysqli_query($db, $name);
-                            $row = mysqli_fetch_row($result);
-                            echo '<a href="#" style="color:#149BBB">' . $row[0] . " " . $row[1] . '</a>
-                            </div>';
-                        } catch(throwable $e){
-                            echo "<p>Error displaying review</p>";
-                        }
-                    ?>
-                </div>
-            </div>
-        </div>
-
-        <div class="carousel-item">
-            <div class="card horizontal sticky-action">
-                <div class="card-stacked">
-                    <div class="card-content">
-                    <span class="material-icons star">star_rate star_rate star_rate star_rate star_rate</span>
-                    <?php
-                        try{
-                            $s = "SELECT review_text FROM review WHERE review_id = 4";
-                            $result = mysqli_query($db, $s);
-                            $row = mysqli_fetch_row($result);
-                            echo "<p>" . $row[0] . "</p>";
-                            echo '</div>
-                            <div class="card-action">';
-                            $name= "SELECT first_name, last_name FROM users u, review r WHERE review_id=4 AND u.user_id=r.user_id";
-                            $result = mysqli_query($db, $name);
-                            $row = mysqli_fetch_row($result);
-                            echo '<a href="#" style="color:#149BBB">' . $row[0] . " " . $row[1] . '</a>
-                            </div>';
-                        } catch(throwable $e){
-                            echo "<p>Error displaying review</p>";
-                        }
-                    ?>
-                </div>
-            </div>
+<div id="review-container">
+    <div class="container">
+        <div class="row" id="title">
+            <h1>What people are <span class="highlight">saying</span></h1>
+            <h2>We've had the pleasure of delivering to over 500 customers worldwide. Here's what they've had to say.<h2>
         </div>
     </div>
-    
-    <?php
-    if (isset($_SESSION["username"]) && (!isset($_POST['review_submit']))){
-        echo '<div class="container"  id="feedback_title">
-                <div class="row">
-                    <h1><br>Tell us what you <span class="highlight">think</span></h1><br>
+
+        <div class="carousel">
+
+            <div class="carousel-item">
+                <div class="card horizontal sticky-action">
+                    <div class="card-stacked">
+                        <div class="card-content">
+                        <span class="material-icons star">star_rate star_rate star_rate star_rate star_rate</span>
+                        <?php
+                            try{
+                                $s = "SELECT review_text FROM review WHERE review_id = 1";
+                                $result = mysqli_query($db, $s);
+                                $row = mysqli_fetch_row($result);
+                                echo "<p>" . $row[0] . "</p>";
+                                echo '</div>
+                                <div class="card-action">';
+                                $name= "SELECT first_name, last_name FROM users u, review r WHERE review_id=1 AND u.user_id=r.user_id";
+                                $result = mysqli_query($db, $name);
+                                $row = mysqli_fetch_row($result);
+                                echo '<a href="#" style="color:#149BBB">' . $row[0] . " " . $row[1] . '</a>
+                                </div>';
+                            } catch(throwable $e){
+                                echo "<p>Error displaying review</p>";
+                            }
+                        ?>
+                    </div>
                 </div>
-                    <form name="review" id="review" action="" method="POST">
+            </div>
+
+            <div class="carousel-item">
+                <div class="card horizontal sticky-action">
+                    <div class="card-stacked">
+                        <div class="card-content">
+                        <span class="material-icons star">star_rate star_rate star_rate star_rate star_rate</span>
+                        <?php
+                            try{
+                                $s = "SELECT review_text FROM review WHERE review_id = 2";
+                                $result = mysqli_query($db, $s);
+                                $row = mysqli_fetch_row($result);
+                                echo "<p>" . $row[0] . "</p>";
+                                echo '</div>
+                                <div class="card-action">';
+                                $name= "SELECT first_name, last_name FROM users u, review r WHERE review_id=2 AND u.user_id=r.user_id";
+                                $result = mysqli_query($db, $name);
+                                $row = mysqli_fetch_row($result);
+                                echo '<a href="#" style="color:#149BBB">' . $row[0] . " " . $row[1] . '</a>
+                                </div>';
+                            } catch(throwable $e){
+                                echo "<p>Error displaying review</p>";
+                            }
+                        ?>
+                    </div>
+                </div>
+            </div>
+
+            <div class="carousel-item">
+                <div class="card horizontal sticky-action">
+                    <div class="card-stacked">
+                        <div class="card-content">
+                        <span class="material-icons star">star_rate star_rate star_rate star_rate star_rate</span>
+                        <?php
+                            try{
+                                $s = "SELECT review_text FROM review WHERE review_id = 3";
+                                $result = mysqli_query($db, $s);
+                                $row = mysqli_fetch_row($result);
+                                echo "<p>" . $row[0] . "</p>";
+                                echo '</div>
+                                <div class="card-action">';
+                                $name= "SELECT first_name, last_name FROM users u, review r WHERE review_id=3 AND u.user_id=r.user_id";
+                                $result = mysqli_query($db, $name);
+                                $row = mysqli_fetch_row($result);
+                                echo '<a href="#" style="color:#149BBB">' . $row[0] . " " . $row[1] . '</a>
+                                </div>';
+                            } catch(throwable $e){
+                                echo "<p>Error displaying review</p>";
+                            }
+                        ?>
+                    </div>
+                </div>
+            </div>
+
+            <div class="carousel-item">
+                <div class="card horizontal sticky-action">
+                    <div class="card-stacked">
+                        <div class="card-content">
+                        <span class="material-icons star">star_rate star_rate star_rate star_rate star_rate</span>
+                        <?php
+                            try{
+                                $s = "SELECT review_text FROM review WHERE review_id = 4";
+                                $result = mysqli_query($db, $s);
+                                $row = mysqli_fetch_row($result);
+                                echo "<p>" . $row[0] . "</p>";
+                                echo '</div>
+                                <div class="card-action">';
+                                $name= "SELECT first_name, last_name FROM users u, review r WHERE review_id=4 AND u.user_id=r.user_id";
+                                $result = mysqli_query($db, $name);
+                                $row = mysqli_fetch_row($result);
+                                echo '<a href="#" style="color:#149BBB">' . $row[0] . " " . $row[1] . '</a>
+                                </div>';
+                            } catch(throwable $e){
+                                echo "<p>Error displaying review</p>";
+                            }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <?php
+        if (isset($_SESSION["username"]) && (!isset($_POST['review_submit']))){
+            echo '<div class="container"  id="feedback_title">
                     <div class="row">
-                        <div class="input-field col s10 offset-s1">
-                            <textarea id="reviewarea" name="reviewarea" class="materialize-textarea" type="text" data-length="500"></textarea>
-                            <label for="reviewarea">Type your review here</label>
+                        <h1><br>Tell us what you <span class="highlight">think</span></h1><br>
+                    </div>
+                        <form name="review" id="review" action="" method="POST">
+                        <div class="row">
+                            <div class="input-field col s10 offset-s1">
+                                <textarea id="reviewarea" name="reviewarea" class="materialize-textarea" type="text" data-length="500"></textarea>
+                                <label for="reviewarea">Type your review here</label>
+                            </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="center-align">
-                        <button class="btn waves-effect waves-light save-button" type="submit" name="review_submit" style="background:#149BBB">Submit</button>
-                        </div><br>
-                    </div>
-                    </form>
+                        <div class="row">
+                            <div class="center-align">
+                            <button class="btn waves-effect waves-light save-button" type="submit" name="review_submit" style="background:#149BBB">Submit</button>
+                            </div><br>
+                        </div>
+                        </form>
+                    </div>';
+
+        }
+        
+
+        if (isset($_POST['review_submit'])){
+            try {
+                $review_text = $_POST["reviewarea"];
+                $username = $_SESSION['username'];
+                $query = "SELECT user_id FROM users WHERE '$username' = login_id";
+                $qresult = mysqli_query($db, $query);
+                $qrow = mysqli_fetch_row($qresult);
+                $user_id = $qrow[0];
+                $review_insert = "INSERT into review(review_text, user_id) values ('$review_text', $user_id)";
+                mysqli_query($db, $review_insert);
+
+                echo '<div class="container" id="thanks">
+                    <h1><br>Thank <span class="highlight">you!</span></h1>
+                    <h2>We appreciate your feedback.<h2>
                 </div>';
 
-    }
-    
-
-    if (isset($_POST['review_submit'])){
-        try {
-            $review_text = $_POST["reviewarea"];
-            $username = $_SESSION['username'];
-            $query = "SELECT user_id FROM users WHERE '$username' = login_id";
-            $qresult = mysqli_query($db, $query);
-            $qrow = mysqli_fetch_row($qresult);
-            $user_id = $qrow[0];
-            $review_insert = "INSERT into review(review_text, user_id) values ('$review_text', $user_id)";
-            mysqli_query($db, $review_insert);
-
-            echo '<div class="container" id="thanks">
-                <h1><br>Thank <span class="highlight">you!</span></h1>
-                <h2>We appreciate your feedback.<h2>
-            </div>';
-
-        } catch(throwable $e){
-            echo "error";
+            } catch(throwable $e){
+                echo "error";
+            }
         }
-    }
-
-    include_once "greyfooter.php";
 
 
-    ?>
 
-    
+        ?>
+
         
+            
+        </div>
     </div>
 </div>
-
 </body>
-
+<?php include "greyfooter.php"?>
 </html>
 
 <script>
