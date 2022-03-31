3b6aa5efb2fe7763c645e0ff91c2cf6dc3a7576b
David Tran <davidtran990@hotmail.com>
2022-03-30 23:15:58 -0400

    finished static pages

--- scs/src/components/shopping/InvoiceComplete.js
+++ scs/src/components/shopping/InvoiceComplete.js
@@ -3,7 +3,16 @@ import '../../css/invoicecomplete.css';
 
 const InvoiceComplete = () => {
   return (
-    <div></div>
+    <div>
+      <div id="invoice-container">
+        <div id="invoice-box">
+            <span><i className="large material-icons">thumb_up</i></span>
+            <h1>Thank you! Your order has been recieved.</h1>
+            <p>You will be recieving a confirmation email with order details.</p>
+            <a className="waves-effect waves-light btn" href="products.php" style={{background: "#149BBB"}}><i className="material-icons left">shopping_basket</i>More Products</a>
+        </div>
+      </div>
+    </div>
   )
 }
 
